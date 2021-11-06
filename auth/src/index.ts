04525cbda911837bbe87125b3ce589cd9e5bbf08
mohamed ahmed abd almageed <mohamedmaim97@gmail.com>
2021-11-06 08:55:31 +0200

    wire the routers with the express app

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,16 +1,19 @@
 import App from 'express';
 import { json } from 'body-parser';
 
+import { loginRouter } from './routes/login';
+import { signupRouter } from './routes/signup';
+import { currentUserRouter } from './routes/current-user';
+import { logoutRouter } from './routes/logout';
+
 const app = App();
 
 app.use(json());
 
-app.use('/api/users/currentuser', async (req, res) => {
-  res.json({
-    id: 1,
-    name: 'John Doe',
-  });
-});
+app.use(loginRouter);
+app.use(signupRouter);
+app.use(currentUserRouter);
+app.use(logoutRouter);
 
 app.listen(3000, () => {
   console.log('Server running on port 3000');
