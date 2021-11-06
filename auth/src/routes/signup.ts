070c32b1cd58d944b7439e673b5f482808a80e3d
mohamed ahmed abd almageed <mohamedmaim97@gmail.com>
2021-11-06 11:47:00 +0200

    add validation checks to signup route

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,38 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
 
 const router = Router();
 
-router.get('/signup', async (req, res) => {});
+const checks = [
+  body('email')
+    .isEmail()
+    .withMessage('Email must be valid'),
+  body('password')
+    .trim()
+    .isLength({ min: 6 })
+    .withMessage('Password must be at least 6 characters long'),
+]
+
+
+const handleValidation = (req: Request, res:Response, next: Function) => {
+  const errors = validationResult(req);
+  if (errors.isEmpty()) {
+    return next();
+  }
+
+  return res.status(422).json({
+    errors,
+  });
+}
+
+
+router.get(
+  '/signup',
+  checks,
+  handleValidation,
+  async (req:Request , res:Response) => {
+
+  },
+);
 
 export { router as signupRouter };
