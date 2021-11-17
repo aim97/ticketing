c30c90ca691b753fab16c88038fa13c96b747753
mohamed ahmed abd almageed <mohamedmaim97@gmail.com>
2021-11-17 21:14:34 +0200

    add current user route

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -2,6 +2,8 @@ import { Router } from 'express';
 
 const router = Router();
 
-router.get('/currentuser', async (req, res) => {});
+router.get('/currentuser', async (req, res) => {
+  res.status(200).send(req.currentUser);
+});
 
 export { router as currentUserRouter };
