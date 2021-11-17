816f718db82f845c0aafcbc2353f5ce3855ef966
mohamed ahmed abd almageed <mohamedmaim97@gmail.com>
2021-11-17 09:30:13 +0200

    fix 13

--- auth/src/db/index.ts
+++ auth/src/db/index.ts
@@ -2,11 +2,7 @@ import mongoose, { ConnectOptions } from 'mongoose';
 import User from './models/user';
 
 const db = {
-  connect: () => mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-  } as ConnectOptions),
+  connect: () => mongoose.connect('mongodb://auth-mongo-srv:27017/auth'),
   disconnect: () => mongoose.disconnect(),
   users: User,
 };
