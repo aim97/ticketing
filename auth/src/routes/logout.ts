f9bffcb996599114b0ffd37fbd41e3650169a5fe
mohamed ahmed abd almageed <mohamedmaim97@gmail.com>
2021-11-17 17:54:07 +0200

    add logout route

--- auth/src/routes/logout.ts
+++ auth/src/routes/logout.ts
@@ -1,7 +1,12 @@
 import { Router } from 'express';
+import httpStatus from 'http-status';
 
 const router = Router();
 
-router.post('/logout', async (req, res) => {});
+router.post('/logout', async (req, res) => {
+  req.session = null;
+
+  res.status(httpStatus.OK).json({});
+});
 
 export { router as logoutRouter };
