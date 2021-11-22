0aaaaee6b9bfdcb07dbed41271dcd72f4f900c88
mohamed ahmed abd almageed <mohamedmaim97@gmail.com>
2021-11-22 15:07:22 +0200

    add test for current user when he is not logged in

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -2,11 +2,19 @@ import httpStatus from 'http-status';
 import request from 'supertest';
 import { app } from '../../app';
 
-it('returns 201 for a successful signup', async () => {
+it('returns 200 OK for if user is logged in and the token is valid', async () => {
   const cookie = await global.getCookies();
 
   return request(app)
     .get('/api/users/currentuser')
     .set('Cookie', cookie)
     .expect(httpStatus.OK);
+});
+
+it('returns 200 OK but user=null if user is not logged in', async () => {
+  const res = await request(app)
+    .get('/api/users/currentuser')
+    .expect(httpStatus.OK);
+
+  expect(res.body.user).toBeNull();
 });
