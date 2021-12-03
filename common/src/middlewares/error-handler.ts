84dae7b265166dd339cfb64aa8fb2b8e8c34bd26
mohamed ahmed abd almageed <mohamedmaim97@gmail.com>
2021-12-03 10:24:27 +0200

    fix a typo

--- common/src/middlewares/error-handler.ts
+++ common/src/middlewares/error-handler.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import { CustomError } from '../errors/CustomError';
 
-export const errorHander = (err:Error, req:Request, res:Response, next:NextFunction) => {
+export const errorHandler = (err:Error, req:Request, res:Response, next:NextFunction) => {
   if (err instanceof CustomError) {
     return res.status(err.statusCode).json({
       errors: err.serializeErrors()
