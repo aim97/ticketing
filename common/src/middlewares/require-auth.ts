a0e14100b78252f9c3753ce4a5d62e86040a148e
mohamed ahmed abd almageed <mohamedmaim97@gmail.com>
2021-12-08 17:35:19 +0200

    fix #27 make the middleware return next() if currentUser is defined

--- common/src/middlewares/require-auth.ts
+++ common/src/middlewares/require-auth.ts
@@ -3,7 +3,7 @@ import { AuthenticationError } from '../errors/AuthenticationError'
 
 export const requireAuth = async (req: Request, res: Response, next: NextFunction) => {
   if (req.currentUser) {
-    next();
+    return next();
   }
 
   throw new AuthenticationError();
