5aa9f04384386b02340b4dc26baf6f051ad566f2
mohamed ahmed abd almageed <mohamedmaim97@gmail.com>
2021-12-08 10:28:08 +0200

    add test for ticket creation

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,9 +1,59 @@
-it('has a route at POST:/api/tickets');
+import request from 'supertest';
+import faker from 'faker';
 
-it('requires authentication, and returns 401 unauthorized when a the request doesn\'t have authorization token');
+import app from '../../app';
 
-it('returns bad request when the ticket is invalid');
+it('has a route at POST:/api/tickets', async () => {
+  const res = await request(app).post('/api/tickets/');
+  expect(res.status).not.toBe(404);
+});
 
-it('returns bad request when price is negative');
+it('requires authentication, and returns 401 unauthorized when a the request doesn\'t have authorization token', async() => {
+  const res = await request(app).post('/api/tickets/');
+  expect(res.status).not.toBe(404);
+});
 
-it('retuns 201 created for valid data and credentials');
+it('returns bad request when the ticket is invalid', async () => {
+  const cookies = global.getCookies();
+  //! empty object is not allowed
+  let res = await request(app)
+    .post('/api/tickets/')
+    .send({})
+    .set('Cookie', cookies);
+  expect(res.status).toBe(400);
+
+  //! both title and price are required
+  res = await request(app)
+    .post('/api/tickets/')
+    .send({title: faker.lorem.word(10)})
+    .set('Cookie', cookies);
+  expect(res.status).toBe(400);
+
+  res = await request(app)
+    .post('/api/tickets/')
+    .send({price: faker.datatype.float({min: 0, precision: 0.01})})
+    .set('Cookie', cookies);
+  expect(res.status).toBe(400);
+
+  //! negative price is not allowed
+  res = await request(app)
+    .post('/api/tickets/')
+    .send({
+      title: faker.lorem.word(10),
+      price: faker.datatype.float({max: -1, precision: 0.01}) //* the max is -1 so the price is always negative
+    })
+    .set('Cookie', cookies);
+  expect(res.status).toBe(400);
+});
+
+it('retuns 201 created for valid data and credentials', async() => {
+  const cookies = global.getCookies();
+  await request(app)
+    .post('/api/tickets/')
+    .send({
+      title: faker.lorem.word(10),
+      price: faker.datatype.float({min: 0, precision: 0.01}) //* the max is -1 so the price is always negative
+    })
+    .set('Cookie', cookies)
+    .expect(201);
+});
